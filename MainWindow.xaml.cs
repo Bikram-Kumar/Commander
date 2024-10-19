6d310a1f5b1e88d23bd19a4fbd6e949af83cab0c
Bikram Kumar <coolbikram0@gmail.com>
2024-10-19 20:18:32 +0530

    created project with display off function

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Windows;
+using System.Runtime.InteropServices;
+
+
+
+namespace Commander
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TurnDisplayOff(object sender, RoutedEventArgs e)
+        {
+            SendMessage(-1, 0x0112, 0xF170, 2);
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+        
+    }
+}
