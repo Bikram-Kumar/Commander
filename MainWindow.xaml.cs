a49f6f5a21e5a03a8243d911124131504b67dd9d
Bikram Kumar <coolbikram0@gmail.com>
2024-10-20 20:15:28 +0530

    added delay and button color change in display off

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿
+﻿using System;
 using System.Windows;
 using System.Runtime.InteropServices;
+using System.Timers;
 
 
 
@@ -11,14 +12,26 @@ namespace Commander
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private static System.Timers.Timer displayOffTimer;
         public MainWindow()
         {
             InitializeComponent();
+            displayOffTimer = new System.Timers.Timer(500);
+            displayOffTimer.Elapsed += SendMessageForDisplayOff;
+            displayOffTimer.AutoReset = false;
         }
 
         private void TurnDisplayOff(object sender, RoutedEventArgs e)
         {
+            displayOffTimer.Start();
+
+        }
+
+        public void SendMessageForDisplayOff(Object source, ElapsedEventArgs e)
+        {
             SendMessage(-1, 0x0112, 0xF170, 2);
+
         }
 
         [DllImport("user32.dll", EntryPoint = "SendMessage")]
